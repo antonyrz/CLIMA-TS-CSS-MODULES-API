88e990a948bf35f539818dcdaf45cbdeac265791
Antonyhz <hernandezantony333@gmail.com>
2026-02-13 06:35:02 -0400

    implementando css modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import styles from './App.module.css'; 
+
 
 function App() {
 
   return (
     <>
-      <h1>Hola</h1>
+      <h1 className={styles.title}>Buscador de Clima</h1>
+   
+      <div className={styles.container}>
+        <p>1</p>
+        <p>2</p>
+      </div>
     </>
   )
 }
